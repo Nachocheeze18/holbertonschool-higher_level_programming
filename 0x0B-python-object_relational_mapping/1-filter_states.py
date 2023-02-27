c8825497220b5cca5285ba2d724767fe4fa67857
Nachocheeze18 <5581@holbertonstudents.com>
2023-02-27 07:09:40 -0800

    list states and filter states

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Task 1: lists all states with a name starting with N from the database
+hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    mysql_username = sys.argv[1]
+    mysql_pwd = sys.argv[2]
+    mysql_dbname = sys.argv[3]
+
+    my_db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        password=mysql_pwd,
+        db=mysql_dbname)
+
+    qry_cursor = my_db.cursor()
+    sql_request = """SELECT *
+                    FROM states
+                    WHERE name LIKE BINARY 'N%'
+                    ORDER BY states.id ASC"""
+    qry_cursor.execute(sql_request)
+    records = qry_cursor.fetchall()
+
+    for element in records:
+        print(element)
+
+    my_db.close()
