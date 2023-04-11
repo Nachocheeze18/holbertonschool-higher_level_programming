40a53e380420d08b592b6a68e0dca324b3851d66
Nachocheeze18 <5581@holbertonstudents.com>
2023-04-11 13:25:13 -0700

    file 2 chnages

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const numArgs = args.length;
-
-if (numArgs === 0) {
-	  console.log("No argument");
-} else if (numArgs === 1) {
-	  console.log("Argument found");
-} else {
-	  console.log("Arguments found");
-}
+const args = process.argv.length;
+console.log(args === 2 ? 'No argument' : args === 3 ? 'Argument found' : 'Arguments found');
