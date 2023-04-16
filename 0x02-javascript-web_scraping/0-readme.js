dfe06ec3a9864abdc7af0c85c6767cdab703a064
Nachocheeze18 <5581@holbertonstudents.com>
2023-04-16 15:19:43 -0700

    files 0 and readme

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,16 +1,12 @@
-import sys
+#!/usr/bin/node
+const fs = require('fs');
 
-# Check if the file path is provided as an argument
-if len(sys.argv) != 2:
-    print("Usage: python file_reader.py <file_path>")
-    sys.exit(1)
+const filePath = process.argv[2];
 
-file_path = sys.argv[1]
-
-try:
-    # Open the file with UTF-8 encoding
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
-except Exception as e:
-    print(f"Error occurred while reading the file: {e}")
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
