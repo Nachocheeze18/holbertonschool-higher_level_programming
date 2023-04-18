c8d2f31ea681bed701aded8045c77b7203481195
Nachocheeze18 <5581@holbertonstudents.com>
2023-04-18 14:39:22 -0700

    test

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-if (process.argv.length < 3) {
-  console.error('Can has file doe? kthx');
-} else {
-  const filePath = process.argv[2];
-
-  fs.readFile(filePath, 'utf8', (error, data) => {
-    if (error) {
-      console.error(` :( An error occurred while reading the file: ${error}`);
-      return;
-    }
+fs.readFile(process.argv[2], 'utf8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
     console.log(data);
-  });
-}
+  }
+});
